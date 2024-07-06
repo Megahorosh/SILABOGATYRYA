d7299ff8c16a4690565b994fd81ead067a208ed5
Никита <nikitval@list.ru>
2024-07-06 23:25:26 +0300

    a bit of movement

--- Assets/Scrips/Character/Player/PlayerInputManager.cs
+++ Assets/Scrips/Character/Player/PlayerInputManager.cs
@@ -10,6 +10,9 @@ public class PlayerInputManager : MonoBehaviour
     PlayerControls playerControls;
 
     [SerializeField] Vector2 movementInput;
+    public float verticalInput;
+    public float horizontalInput;
+    public float moveAmount;
 
     private void Awake()
     {
@@ -57,5 +60,27 @@ public class PlayerInputManager : MonoBehaviour
     private void OnDestroy()
     {
         SceneManager.activeSceneChanged -= OnSceneChange;
+    }
+
+    private void Update()
+    {
+        HandleMovementInput();
+    }
+
+    private void HandleMovementInput()
+    {
+        verticalInput = movementInput.y;
+        horizontalInput = movementInput.x;
+
+        moveAmount = Mathf.Clamp01(Mathf.Abs(verticalInput) + Mathf.Abs(horizontalInput));
+
+        if (moveAmount <= 0.5f && moveAmount > 0)
+        {
+            moveAmount = 0.5f;
+        }
+        else if (moveAmount > 0.5f && moveAmount <= 1) 
+        {
+            moveAmount = 1;
+        }
     }
 }
--- Assets/Scrips/Character/Player/PlayerLocomotionManager.cs
+++ Assets/Scrips/Character/Player/PlayerLocomotionManager.cs
@@ -2,11 +2,53 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerLocomotionManager : MonoBehaviour
+public class PlayerLocomotionManager : CharacterLocomotionManager
 {
+    PlayerManager player;
+
+    public float verticalMovement;
+    public float horizontalMovement;
+    public float moveAmount;
+
+    private Vector3 moveDirection;
+    [SerializeField] float walkingSpeed = 2;
+    [SerializeField] float runningSpeed = 5;
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        player = GetComponent<PlayerManager>();
+    }
+
     public void HandleAllMovement()
     {
-        // GROUNDED MOVEMENT
+        HandleGroundedMovement();
         // AERIAL MOVEMENT
+    }
+
+    private void GetVerticalAndHorizontalInputs()
+    {
+        verticalMovement = PlayerInputManager.Instance.verticalInput;
+        horizontalMovement = PlayerInputManager.Instance.horizontalInput;
+        // CLAMP
+    }
+
+    private void HandleGroundedMovement()
+    {
+        GetVerticalAndHorizontalInputs();
+        moveDirection = PlayerCamera.instance.transform.forward * verticalMovement;
+        moveDirection = moveDirection + PlayerCamera.instance.transform.right * horizontalMovement;
+        moveDirection.Normalize();
+        moveDirection.y = 0;
+
+        if (PlayerInputManager.Instance.moveAmount > 0.5f)
+        {
+            player.characterController.Move(moveDirection * runningSpeed * Time.deltaTime);
+        }
+        else if (PlayerInputManager.Instance.moveAmount <= 0.5f)
+        {
+            player.characterController.Move(moveDirection * walkingSpeed * Time.deltaTime);
+        }
     }
 }
